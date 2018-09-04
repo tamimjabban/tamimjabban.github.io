710927e58c8b0239f411dab6cc56db7b9cc8171f
Tamim Jabban <tamimjabban@live.com>
2018-09-04 21:34:46 +0300

    V0.3

--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         if (scroll > 200) {
             $(".name").removeClass("hide");
             $(".navbar").addClass("bg-custom");
 
-        //   $(".name").css("display" , "inline");
-        }
-  
-        else{
-            
+            //   $(".name").css("display" , "inline");
+        } else {
+
             $(".name").addClass("hide");
             $(".navbar").removeClass("bg-custom");
 
         }
-    })
-  })
+    });
+
+    $('.navbar-nav a').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $(event.delegateTarget.hash).offset().top - 150
+        }, 0);
+    });
+})
